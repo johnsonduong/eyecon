1d4d2e679c1bedb1c15dc8feeba99b85f40f25e3
Johnson Duong <71087545+johnsonduong@users.noreply.github.com>
2022-06-04 17:14:45 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,20 @@
+const menu = document.querySelector(".menu");
+const close = document.querySelector(".close");
+const nav = document.querySelector("nav");
+
+menu.addEventListener("click", () => {
+  nav.classList.add("open-nav");
+});
+
+close.addEventListener("click", () => {
+  nav.classList.remove("open-nav");
+});
+
+let resizeTimer;
+window.addEventListener("resize", () => {
+  document.body.classList.add("resize-animation-stopper");
+  clearTimeout(resizeTimer);
+  resizeTimer = setTimeout(() => {
+    document.body.classList.remove("resize-animation-stopper");
+  }, 400);
+});
